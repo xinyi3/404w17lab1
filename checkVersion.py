5a2be7d44a111baaa18bb7f9884d9b7596b138f1
xinyi3 <xinyi3@ualberta.ca>
2017-01-10 09:03:50 -0700

    get itself

--- checkVersion.py
+++ checkVersion.py
@@ -4,7 +4,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/xinyi3/404w17lab1/master/checkVersion.py")
 
 print response.status_code
 
